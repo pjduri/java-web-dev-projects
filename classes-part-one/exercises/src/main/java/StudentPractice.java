fb656c931e44fea97994069f879f2c191fdb9b5a
Your Name <108307350+pjduri@users.noreply.github.com>
2024-01-09 09:33:42 -0600

    ch 4 studio Student class

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,10 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+        Student student = new Student("Your Name", 12345, 1, 4.0);
+
+        System.out.println(student.getName());
+
+
     }
 }
